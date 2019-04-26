d598ae141947a7854925d4a3a7814dc7b214e036
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 05:25:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,10 +2,10 @@ var playlist = {"Artist": "Song"};
 
 function updatePlaylist(playlist, artist, song)
 {
-  playlist[artist] = song;
+  playlist.artist = song;
 }
 
 function removeFromPlaylist(playlist, artist)
 {
-  delete playlist[artist];
+  delete playlist.artist;
 }
